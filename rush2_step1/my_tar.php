2d22dac727b419ffa4d9e1631ba2b11b8b6f853e
louohs <louisa.hadjili@epitech.eu>
2022-12-03 11:18:33 +0100

    rush2

---
+++ rush2_step1/my_tar.php
@@ -0,0 +1,7 @@
+<?php
+
+// LZW (pour Lempel-Ziv-Welch) est un algorithme de compression de données sans perte. Il s'agit d'une amélioration de l'algorithme LZ78 inventé par Abraham Lempel et Jacob Ziv en 1978.
+// 
+function dictionnaire() {
+    
+}
